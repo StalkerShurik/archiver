a3b4d197c9da3a0358a19d95208d41681a827e13
StalkerShurik <67455670+StalkerShurik@users.noreply.github.com>
2021-10-11 00:52:22 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/archiver.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/Programming/CLion Projects/archiver/library/compressor.cpp" "/mnt/d/Programming/CLion Projects/archiver/cmake-build-debug/CMakeFiles/archiver.dir/library/compressor.cpp.o"
+  "/mnt/d/Programming/CLion Projects/archiver/library/decompressor.cpp" "/mnt/d/Programming/CLion Projects/archiver/cmake-build-debug/CMakeFiles/archiver.dir/library/decompressor.cpp.o"
+  "/mnt/d/Programming/CLion Projects/archiver/library/input_process.cpp" "/mnt/d/Programming/CLion Projects/archiver/cmake-build-debug/CMakeFiles/archiver.dir/library/input_process.cpp.o"
+  "/mnt/d/Programming/CLion Projects/archiver/library/my_heap.cpp" "/mnt/d/Programming/CLion Projects/archiver/cmake-build-debug/CMakeFiles/archiver.dir/library/my_heap.cpp.o"
+  "/mnt/d/Programming/CLion Projects/archiver/main.cpp" "/mnt/d/Programming/CLion Projects/archiver/cmake-build-debug/CMakeFiles/archiver.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/archiver.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/archiver.dir/library/compressor.cpp.o"
+  "CMakeFiles/archiver.dir/library/decompressor.cpp.o"
+  "CMakeFiles/archiver.dir/library/input_process.cpp.o"
+  "CMakeFiles/archiver.dir/library/my_heap.cpp.o"
+  "CMakeFiles/archiver.dir/main.cpp.o"
+  "archiver"
+  "archiver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/archiver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
